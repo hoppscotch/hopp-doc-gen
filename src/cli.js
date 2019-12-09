7c12a5ee1752c79b6e27957ad993723803ed5970
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-12-09 15:38:54 +0530

    feat: configure ascii banner

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,8 @@
 
 'use strict'
 
+const showBanner = require('node-banner');
+
 const {
   showHelpInformation,
   showVersionInformation,
@@ -12,7 +14,9 @@ const {
 const [, , ...args] = process.argv;
 
 if (!args.length) {
-  console.log('Postwoman CLI', 'A CLI solution for Postwoman');
+  (async () => {
+    await showBanner('Postwoman CLI', 'A CLI solution for Postwoman');
+  })();
 } else if (args.length > 1 || !args[0].includes('-')) {
   showInvalidArgsInformation();
 } else if (['--help', '-h'].includes(args[0])) {
