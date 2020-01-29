2504cf4f41d5de67a2e968c9a9f5649a4c4b0ea6
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-01-29 21:40:14 +0530

    refactor: switch to commander.js implementation

--- src/cli.js
+++ src/cli.js
@@ -2,27 +2,16 @@
 
 'use strict'
 
-const {
-  showHelpInformation,
-  showVersionInformation,
-  showUnknownOptionInformation
-} = require('./utils/helpers')
+const program = require('commander')
 
+const { description, version } = require('../package')
 const generateAPIDoc = require('./commands/generate')
 
-// parse args
-const [, , ...args] = process.argv
+program
+  .description(description)
+  .version(version)
 
-if (!args.length) {
-  ;(async () => {
-    await showHelpInformation()
-  })()
-} else if (['--help', '-h'].includes(args[0])) {
-  showHelpInformation()
-} else if (['--version', '-v'].includes(args[0])) {
-  showVersionInformation()
-} else if (args[0] === 'generate') {
-  generateAPIDoc(args)
-} else {
-  showUnknownOptionInformation(args[0])
+if (!program._args.length) {
+  program.outputHelp()
+  process.exit(1)
 }
