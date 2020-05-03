809701700b5ba1a41882878025bd100e130047de
yubathom <yubathom@gmail.com>
2020-05-03 00:47:32 -0300

    feat: format json generated doc

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -55,6 +55,52 @@ const generateAPIDoc = async filePath => {
 
   let idx = 1
 
+  /**
+   * Template for the docs/README.md for the json parser
+   */
+  const tp = {
+    /**
+     * Check if a json value is empty
+     * @param { JSON } ct The hdg spec-collection.json value
+     */
+    isEmpty: ct => ct.length === 0 || Object.keys(ct).length === 0,
+    /**
+     * Return a prettier string for json values
+     * @param { String } key The hdg spec-collection.json key
+     * @param { String } ct The hdg spec-collection.json value
+     */
+    prettyJson: (key, ct) => {
+      return (
+        ` - ${key}:` + '\n```json\n' + JSON.stringify(ct, null, 2) + '\n```'
+      )
+    },
+    /**
+     * Return a prettier string for javascript values
+     * @param { String } key The hdg spec-collection.json key
+     * @param { String } ct The hdg spec-collection.json value
+     */
+    prettyJs: (key, ct) => ` - ${key}:` + '\n```javascript\n' + ct + '\n```',
+    url: (key, ct) => ` - ${key}: ${ct}`,
+    path: (key, ct) => ` - ${key}: ${ct}`,
+    method: (key, ct) => ` - ${key}: ${ct}`,
+    auth: (key, ct) => (ct === 'None' ? '' : ` - ${key}: ${ct}`),
+    httpUser: (key, ct) => ` - ${key}: ${ct}`,
+    httpPassword: (key, ct) => ` - ${key}: ${ct}`,
+    passwordFieldType: (key, ct) => ` - ${key}: ${ct}`,
+    bearerToken: (key, ct) => ` - ${key}: ${ct}`,
+    contentType: (key, ct) => ` - ${key}: ${ct}`,
+    requestType: (key, ct) => ` - ${key}: ${ct}`,
+    rawParams: (key, ct) => {
+      const parsed = JSON.parse(ct)
+      return tp.isEmpty(parsed) ? '' : tp.prettyJson(key, parsed)
+    },
+    headers: (key, ct) => (tp.isEmpty(ct) ? '' : tp.prettyJson(key, ct)),
+    params: (key, ct) => (tp.isEmpty(ct) ? '' : tp.prettyJson(key, ct)),
+    bodyParams: (key, ct) => (tp.isEmpty(ct) ? '' : tp.prettyJson(key, ct)),
+    preRequestScript: (key, ct) => tp.prettyJs(key, ct),
+    testScript: (key, ct) => tp.prettyJs(key, ct)
+  }
+
   data.forEach(item => {
     idx++
     apiDoc[idx] = `## ${item.name}`
@@ -62,11 +108,15 @@ const generateAPIDoc = async filePath => {
       idx++
       apiDoc[idx] = `### ${request.name}`
       Object.keys(request)
-        .filter(key => key !== 'name')
+        .filter(key => tp[key] && request[key])
         .forEach(key => {
-          if (request[key]) {
+          try {
             idx++
-            apiDoc[idx] = `- ${key}: ${request[key]}`
+            apiDoc[idx] = tp[key](key, request[key])
+          } catch (err) {
+            logError(
+              `\n Check key: ${key}. Make sure that postwoman-collection.json schema is correct`
+            )
           }
         })
       idx++
