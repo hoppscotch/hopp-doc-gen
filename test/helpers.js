edc71896871c0c776d4e152196343cf258cae812
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-09-03 12:58:14 +0530

    chore: add JSDoc compliant method signature

--- test/helpers.js
+++ test/helpers.js
@@ -3,6 +3,12 @@ const path = require('path')
 
 const CLI_PATH = path.join(__dirname, '..', 'index.js')
 
+/**
+ * Execute shell commands
+ * @param {String[]} args args to be supplied
+ * @param {Object} options execa options
+ * @returns {Object}
+ */
 const run = (args, options = {}) => execa.sync(CLI_PATH, args, options)
 
 module.exports = run
